62afe43e5df42a29e8bacc614107f89b73f14391
Sean Tseng <n31629@gmail.com>
2020-01-07 00:55:46 +0800

    second-vision

--- keep_accounts.py
+++ keep_accounts.py
@@ -1,10 +1,23 @@
 products = [] 
+with open('products.csv', 'r', encoding = 'Big5') as f:
+    for line in f:
+    	if '商品, 價格' in line:
+    		continue
+        name, price = line.strip().split(',')
+        products.append([name, price])
+
 while True:
     name = input('請輸入商品名稱：')
     if name == 'q':
         break
     price = input('請輸入商品價格：')
+    price = int(price)
     products.append([name, price])
 
 for p in products:
 	print(p[0], '的價格是：', p[1], '元')
+
+with open('products.csv', 'w', encoding = 'Big5') as f:
+    f.write('商品, 價格\n')
+    for p in products:
+        f.write(p[0] + ',' + str(p[1]) + '\n')
